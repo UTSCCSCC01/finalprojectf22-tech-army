f4be2b05121798e31a2fcc4d2675df9abf6eb0b5
Andrew Qian <andrewqian2001@gmail.com>
2022-09-28 23:51:11 -0400

    add user registration and password hashing

--- server/routes/users.js
+++ server/routes/users.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const User = require("../models/User");
+const bcrypt = require('bcryptjs');
 
 
 //http://localhost:8000/api/users
@@ -18,10 +19,26 @@ router.post('/', [
         return value.includes("@mail.utoronto.ca");
     }),
     check('password', 'Please enter a password with 5 or more characters').isLength({min: 5})
-], (request, response) => { 
+], async (request, response) => { 
     const errors = validationResult(request);
     if(!errors.isEmpty()) return response.status(400).json({errors: errors.array()});
-    response.send("huzzah");
+    const {name, email, password} = request.body;
+    try{
+        let user = await User.findOne({email: email});
+        if(user) return response.status(400).json({message: 'Email is already registered'});
+        user = new User({
+            name,
+            email,
+            password
+        });
+        const salt = await bcrypt.genSalt() //encrypt the password
+        user.password = await bcrypt.hash(password, salt);
+        await user.save();
+        response.send("User is registered");
+    }catch(error){
+        console.log(error.message);
+        response.send(500); //server error
+    }
 }); 
 
 module.exports = router;
