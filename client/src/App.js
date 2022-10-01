67a49c74447982765e0585cf3fc5f3095181ef49
JohnY2002 <johnyuan0526@gmail.com>
2022-10-01 17:28:55 -0400

    Interface Finished
    Finished the lay out of the log in page. Still need connect it to the database to get all the information needed.

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import LoginForm from './components/LoginForm';
 
 function App() {
+
+// Getting email address and password from database
+const adminUser = {
+  name: "John",
+  email: "admin@admin.com",
+  password: "admin123"
+}
+
+// Set the user name and user email once logged in
+const [user, setUser] = useState({name: "", email: ""});
+// Error checking
+const [error, setError] = useState("");
+
+// Login Method 
+const Login = details => {
+  console.log(details);
+
+  if (details.email === adminUser.email && details.password === adminUser.password){
+    console.log("Logged in");
+    setUser({
+      name: details.name,
+      email: details.email
+    });
+  }else{
+    console.log("Email or password is wrong!");
+    setError("Email or password is wrong!");
+  }
+}
+
+const Logout = () => {
+  setUser({email: ""});
+}
+
+  // Display information once the user is logged in
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {(user.email !== "") ? (
+        <div className="welcome">
+          <h2>Welcome, <span>{user.name}</span></h2>
+          <button onClick={Logout}>Logout</button>
+        </div>
+      ) : (
+        <LoginForm Login={Login} error={error}/>
+      )}
     </div>
   );
 }
