ad3753496be8d5061d34800d0a5e5c6ec464ff6e
JohnY2002 <johnyuan0526@gmail.com>
2022-10-02 23:54:53 -0400

    TA-10 added login in progress

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route, NavLink, } from "react-router-dom";
+
+import Login from "./Login";
+import Dashboard from "./Dashboard";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <div className="header">
+          <NavLink activeclassname="active" to="/">Login</NavLink>
+          <NavLink activeclassname="active" to="/dashboard">Dashboard</NavLink>
+        </div>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Login/>} />
+            <Route path="/dashboard" element={<Dashboard/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
     </div>
   );
 }
