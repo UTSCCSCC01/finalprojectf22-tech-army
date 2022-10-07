be891f639ba21533afe2ae351e0c3d1d69eccc83
Muntaqa Mahmood <69706702+muntaqamahmood@users.noreply.github.com>
2022-10-07 01:21:16 -0400

    TA-9: Created event model and routing for events

---
+++ server/models/Event.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+
+const EventSchema = mongoose.Schema({
+    //title of the event
+    title: {
+        type: String,
+        required: true,
+    },
+    //may need to change required to false as it is not required, not sure yet.
+    description: {
+        type: String,
+        required: true,
+    },
+    //date is a string because it is easier to work with
+    date: {
+        type: Date,
+        required: true,
+    },
+    //use the user model to create a relationship between the user and the event
+    creator: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+});
+
+module.exports = mongoose.model('event', EventSchema);
+
