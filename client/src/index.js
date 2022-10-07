b6b840a640c4de4e5b4a50eeb3e6ad079113fadf
Nandhakishore K.S <n.krishnamurthy@mail.utoronto.ca>
2022-10-07 03:23:04 -0400

    TA-5/Created the base for sidebar

--- client/src/index.js
+++ client/src/index.js
@@ -7,14 +7,25 @@ import './index.css';
 import Login from './Login';
 import reportWebVitals from './reportWebVitals';
 import Home from './Pages/Home';
+import Navbar from './Components/Navbar';
+import ProfileInfo from './Components/profileInfo';
+import Events from './Pages/events';
+import Market from './Pages/market';
+import Message from './Pages/message';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
+  <Navbar />
     <Routes>
       <Route path="/" exact element={<Login />}/>
       <Route path="/dashboard" element={<Home/>} />
       <Route path="/signup" exact element={<Signup />}/>
+      <Route path="/profile" exact element={<ProfileInfo />}/>
+      <Route path="/market" exact element={<Market />}/>
+      <Route path="/events" exact element={<Events />}/>
+      <Route path="/message" exact element={<Message />}/>
     </Routes>
   </BrowserRouter>
 );
