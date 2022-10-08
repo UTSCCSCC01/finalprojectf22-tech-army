14be2372bf10697363c9c5e15e1819205f5c4909
Henry-Zhuo <henryzhuo@live.com>
2022-10-07 21:13:02 -0400

    TA-5: Grab changes from old TA-5 branch
    - original merge by n.krishnamurthy@mail.utoronto.ca, using TA-5-fixed, while TA-5 branch was off of old develop
    - Includes .js files for pages, css, login_pages/, and Dashboard.js

---
+++ client/src/Pages/about.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '../index.js'
+import './show.css'
+  
+const About = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'Left',
+        alignItems: 'Left',
+        height: '100vh',
+        paddingLeft: "25%"
+      }}
+    >
+      <h1>Welcome to About Us Page!</h1>
+    </div>
+  );
+};
+  
+export default About;
--- client/src/index.js
+++ client/src/index.js
@@ -2,30 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Signup from './Signup';
+import Signup from './login_pages/Signup';
 import './index.css';
-import Login from './Login';
-import reportWebVitals from './reportWebVitals';
-import Home from './Pages/Home';
-import Navbar from './Components/Navbar';
-import ProfileInfo from './Components/profileInfo';
-import Events from './Pages/events';
-import Market from './Pages/market';
-import Message from './Pages/message';
-
+import Login from './login_pages/Login';
+import Dashboard from './Dashboard';
+import Navbar from './Navbar';
+import About from './pages/about';
+import Events from './pages/events';
+import Market from './pages/market';
+import Message from './pages/message';
+import Settings from './pages/settings';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
-  <Navbar />
+    <Navbar />
     <Routes>
       <Route path="/" exact element={<Login />}/>
-      <Route path="/dashboard" element={<Home/>} />
+      <Route path="/dashboard" element={<Dashboard/>} />
       <Route path="/signup" exact element={<Signup />}/>
-      <Route path="/profile" exact element={<ProfileInfo />}/>
+      <Route path="/about" exact element={<About />}/>
       <Route path="/market" exact element={<Market />}/>
       <Route path="/events" exact element={<Events />}/>
       <Route path="/message" exact element={<Message />}/>
+      <Route path="/settings" exact element={<Settings />}/>
     </Routes>
   </BrowserRouter>
 );
