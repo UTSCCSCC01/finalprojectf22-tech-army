f70e70dcfb8acf042a7af91760f052db9300fc56
Shence <ysc1412799032@163.com>
2022-10-20 00:10:06 -0400

    testing bug

--- client/src/Pages/Events.jsx
+++ client/src/Pages/Events.jsx
@@ -1,30 +1,174 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useEffect } from 'react';
+import axios from "axios";
+import { Col, Card, Row } from 'antd';
+import ImageSlider from '../Utils/ImageSlider';
+import Button from '@mui/material/Button'
+
 import { useNavigate } from "react-router-dom";
 
 const Events = () => {
 
   const navigate = useNavigate();
 
-  const handlePost = () => {
+  const handlePost =() => {
     navigate('/events/postevent');
-  }
+}
+
+  const [Events, setevents] = useState([])
+  const { Meta } = Card;
+
+  const [Skip, setSkip] = useState(0)
+  const [Limit, setLimit] = useState(8)
   
+  // only when the PostSize is 8 then we show the load more button
+  const [PostSize, setPostSize] = useState()
+
+
+
+  useEffect( () => {
+
+    const body = {
+      skip: Skip,
+      limit : Limit
+
+    }
+
+    axios.post('http://localhost:8000/api/events',body).then (response =>{
+      if (response.data.success) {
+
+            setevents([...response.data.event])     //check if the name is called Events
+
+            setPostSize(response.data.PostSize)
+            console.log()
+      } else{
+          alert('cant fetch data from db')
+      }
+    })
+
+  }, [])
+
+
+
+
+  const Loadfunc = () =>{
+      let skip = Skip + Limit;
+
+      const body = {
+            skip: skip,
+            limit : Limit
+
+      }
+
+          // need to update 
+          axios.post('http://localhost:8000/api/events',body).then (response =>{
+      if (response.data.success) {
+
+            setevents([...response.data.event])     //check if the name is called Events
+
+            setPostSize(response.data.PostSize)
+            console.log()
+      } else{
+          alert('cant fetch data from db')
+      }
+    })
+
+
+    setSkip(skip)
+
+  }
+
+
+
+
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'Left',
-        alignItems: 'Left',
-        paddingLeft: "25%"
-      }}
-    >
-      <h1>Welcome to Events Page!</h1>
-      <input
-        type="button"
-        value={"Post Event"}
-        onClick={handlePost}
-      />
-    </div>
+    <>
+       <div
+          style={{
+          display: 'flex',
+          justifyContent: 'Left',
+          alignItems: 'Left',
+          //height: '100vh',
+          paddingLeft: "45%"
+        }}
+       >
+              <h1>Welcome to Events Page!</h1> 
+      </div>
+
+      <div
+          style={{
+          alignItems: 'Left',
+          //height: '100vh',
+          paddingLeft: "45%"
+        }}
+       >
+
+
+
+
+          <Button variant="contained" size="large" color="secondary" onClick={handlePost}>Post event</Button>
+
+          {Events.length === 0 ?
+                <div style={{ display: 'flex', height: '300px', justifyContent: 'Left', alignItems: 'Left' }}>
+                    <h2>No events yet...</h2>
+                </div> :
+
+                                // render card part
+                <div>
+                    
+                     <Row gutter={[16, 16]}></Row>
+                        {Events.map((events,index) => {
+                            return <Col lg={6} md={8} xs={24}>
+
+                                    <Card
+                                          hoverable={true}
+                                          cover={
+                                             <ImageSlider images={events.images} />}
+                                    >
+
+                                        <Meta
+                                            title={events.title}
+                                            description={events.description}
+                                        />
+                                   </Card>
+
+                                  </Col>
+                        }) }
+
+
+                </div>
+            }
+
+            <br /><br />
+
+
+            { PostSize >= Limit &&
+                <div style={{ display: 'flex', justifyContent: 'Left' }}>
+                    <button onClick={Loadfunc}>Load More</button>
+                </div>
+            }
+          
+            
+
+            
+               
+
+
+
+
+
+
+
+
+
+
+        </div>
+   
+    
+    </>
+    
+
+    
   );
 };
   
