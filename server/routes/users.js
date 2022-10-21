3a2477528f0b9cef9b64ce22f1e194afc3103f69
Andrew Qian <andrewqian2001@gmail.com>
2022-10-21 12:20:00 -0400

    TA-27: find by id

--- server/routes/users.js
+++ server/routes/users.js
@@ -60,14 +60,13 @@ router.put('/', auth, async (request, response) =>{
     const errorMessages = validationResult(request);
     if(!errorMessages.isEmpty()) return response.status(400).json({errors: errorMessages.array()});
     try {
-        const {name, email, password, profilePictureURL} = request.body;
-        const filter = {email};
+        const {name, password, profilePictureURL} = request.body;
         const salt = await bcrypt.genSalt();
         let newPass;
         if(password) newPass = await bcrypt.hash(password, salt);
         const update = {name: name, password: newPass, profilePictureURL: profilePictureURL};
         Object.keys(update).forEach(key => update[key] === undefined ? delete update[key] : {});
-        await User.findOneAndUpdate(filter, update);
+        await User.findByIdAndUpdate(request.user.id, update);
         response.json({message: "Updated user info"});
     } catch(error) {
         console.log(error.message);
