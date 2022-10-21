7923d93466e3531ee02aee67c847bcb2d2e59931
JohnY2002 <johnyuan0526@gmail.com>
2022-10-20 21:51:27 -0400

    TA-34: Added upload.js to the server routes so it is more clear and can be reused.

--- client/src/Pages/Events.jsx
+++ client/src/Pages/Events.jsx
@@ -1,79 +1,15 @@
-import React from 'react';
+import React, { useState } from "react";
 import Navbar from '../Components/Navbar/Navbar';
+import { useNavigate } from "react-router-dom";
   
 const Events = () => {
 
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
 
-  const handlePost =() => {
+  const handleCreate = () => {
     navigate('/events/postevent');
-}
-
-  const [Events, setevents] = useState([])
-  const { Meta } = Card;
-
-  const [Skip, setSkip] = useState(0)
-  const [Limit, setLimit] = useState(8)
-  
-  // only when the PostSize is 8 then we show the load more button
-  const [PostSize, setPostSize] = useState()
-
-
-
-  useEffect( () => {
-
-    const body = {
-      skip: Skip,
-      limit : Limit
-
-    }
-
-    axios.post('http://localhost:8000/api/events',body).then (response =>{
-      if (response.data.success) {
-
-            setevents([...response.data.event])     //check if the name is called Events
-
-            setPostSize(response.data.PostSize)
-            console.log()
-      } else{
-          alert('cant fetch data from db')
-      }
-    })
-
-  }, [])
-
-
-
-
-  const Loadfunc = () =>{
-      let skip = Skip + Limit;
-
-      const body = {
-            skip: skip,
-            limit : Limit
-
-      }
-
-          // need to update 
-          axios.post('http://localhost:8000/api/events',body).then (response =>{
-      if (response.data.success) {
-
-            setevents([...response.data.event])     //check if the name is called Events
-
-            setPostSize(response.data.PostSize)
-            console.log()
-      } else{
-          alert('cant fetch data from db')
-      }
-    })
-
-
-    setSkip(skip)
-
   }
-
-
-
 
   return (
     <><Navbar />
@@ -87,6 +23,12 @@ const Events = () => {
       }}
     >
       <h1>Welcome to Events Page!</h1>
+      <input
+        type="button"
+        value={loading ? "Loading..." : "Create"}
+        disabled={loading}
+        onClick={handleCreate}
+      />
     </div></>
   );
 };
