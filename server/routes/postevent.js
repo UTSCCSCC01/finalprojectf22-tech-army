c86c4ba5bee3b3800a51aaa67daee527a2d33238
ksnandz <41064291+ksnandz@users.noreply.github.com>
2022-10-21 15:43:48 -0400

    TA-35/Added search bar functionality (#52)
    * TA-34: Added post event interface
    
    * TA-34: updated the post event interface
    
    * TA-34: Tried to add the upload picture for event feature, work in progress
    
    * TA-34: Adding insert event picture feature. Working in progress
    
    * TA-34: Make err the only input to the function given to upload
    
    * TA-34: Add uploads/ folder to be ignored in server/ dir
    
    * TA-34: Add gitignore to server/uploads/ folder to ensure only the folder and gitignore is in repo
    
    - replaces ignoring entire folder as folder is needed for files to be saved
    
    * TA-34: testing
    
    * testing bug
    
    * TA-34:set up event page to load and display event WIP
    
    * TA-34: finish post event and set up backend events model also required api
    
    * Update yarn.lock
    
    * TA-41: added the event detail page, work in progress.
    
    * TA-41: Added the event detail page.
    
    * TA-35/Added search bar functionality
    
    * Delete package-lock.json
    
    Co-authored-by: JohnY2002 <johnyuan0526@gmail.com>
    Co-authored-by: Henry-Zhuo <henryzhuo@live.com>
    Co-authored-by: Shence <ysc1412799032@163.com>
    Co-authored-by: JohnY2002 <94477598+JohnY2002@users.noreply.github.com>

--- server/routes/postevent.js
+++ server/routes/postevent.js
@@ -35,6 +35,24 @@ router.post("/getEvents", auth, (req, res) => {
 });
 
 
+router.get("/events_by_id", auth, (req, res) => {
+    let type = req.query.type
+    let eventIds = req.query.id
+
+    if (type === "array") {
+
+    }
+
+
+    //we need to find the product information that belong to product Id 
+    Event.find({ '_id': { $in: eventIds } })
+        .populate('writer')
+        .exec((err, event) => {
+            if (err) return res.status(400).send(err)
+            return res.status(200).send(event)
+        })
+});
+
 
 
 module.exports = router;
