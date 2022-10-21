a443b5b7b121a5f5976bd7ee2398b2fcddfbbf7b
Henry-Zhuo <henryzhuo@live.com>
2022-10-21 01:47:30 -0400

    TA-40: Add save and cancel buttons and functionality for editing profile info

--- client/src/Pages/EditProfile.jsx
+++ client/src/Pages/EditProfile.jsx
@@ -1,7 +1,49 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
+import Button from '@mui/material/Button';
+import {
+  getToken,
+  setUserData,
+} from '../Utils/Common';
 import Navbar from '../Components/Navbar/Navbar';
+import axios from "axios";
 
 const EditProfile = () => {
+
+  const navigate = useNavigate();
+
+  const saveChanges = () => {
+    const body = {
+      email: email,
+      password: password,
+      username: username,
+    };
+
+    let axiosConfig = {
+      headers: {
+        'Content-Type': 'application/json;charset=UTF-8',
+        'x-auth-token': getToken(),
+      }
+    };
+
+    axios.post('/api/users', body, axiosConfig).then(response => {
+      if (response.data.success) {
+        // Succesfully changed account info on backend, update data on client
+        setUserData(username, email);
+        navigate('/dashboard');
+      } else {
+        const errorMsg = 'Error while saving changes';
+        alert(errorMsg);
+        console.error(errorMsg);
+        console.error(response);
+      }
+    });
+  }
+
+  const cancelChanges = () => {
+    navigate('/dashboard');
+  }
+
   return (
     <>
     <Navbar />
@@ -15,6 +57,9 @@ const EditProfile = () => {
       }}
     >
       <h1>Edit Profile</h1>
+
+      <Button variant="contained" size="large" color="secondary" onClick={saveChanges}>Save</Button>
+      <Button variant="contained" size="large" color="secondary" onClick={cancelChanges}>Cancel</Button>
     </div>
     </>
   );
