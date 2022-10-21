32fa082078ace4d02ab5ebaa3cd66f72190891b3
Henry-Zhuo <henryzhuo@live.com>
2022-10-21 03:01:08 -0400

    TA-40: Add support for setting and getting profilePicUrl
    - Use profilepic1.png as the default profile pic if none is found

--- client/src/Utils/Common.js
+++ client/src/Utils/Common.js
@@ -1,3 +1,5 @@
+import defaultProfilePic from'../assets/profilepic1.png'
+
 export const getUser =() => {
     const userStr = sessionStorage.getItem("name");
 
@@ -17,6 +19,12 @@ export const getEmail =() => {
     else return null;
 }
 
+export const getProfilePicUrl = () => {
+    const profilePicUrl = sessionStorage.getItem("profilePicUrl");
+
+    if (profilePicUrl) return JSON.parse(profilePicUrl);
+    else return defaultProfilePic;
+}
 
 export const getToken =() => {
     return sessionStorage.getItem("token") || null;
@@ -27,9 +35,10 @@ export const setUserSession = (token) => {
 }
 
 
-export const setUserData = (name,email) => {
+export const setUserData = (name, email, profilePicUrl) => {
     sessionStorage.setItem("name", JSON.stringify(name));
     sessionStorage.setItem("email", JSON.stringify(email));
+    sessionStorage.setItem("profilePicUrl", JSON.stringify(profilePicUrl));
 }
 
 
