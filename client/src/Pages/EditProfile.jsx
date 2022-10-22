db24b644fb07fedfc7cba4c16723262f7bc47a17
Henry Zhuo <44787781+Henry-Zhuo@users.noreply.github.com>
2022-10-21 23:45:49 -0400

    TA-44: Find user by email to change account info (#62)
    * TA-44: Find user by email instead of id
    
    - Revert "TA-27: find by id"
    
    This reverts commit 3a2477528f0b9cef9b64ce22f1e194afc3103f69.
    
    * TA-44: Check if response status is 200 to accept account info update
    
    * TA-44: Find user by id instead of email
    
    - user id is included in request by default
    
    - Revert "TA-44: Find user by email instead of id"
    
    This reverts commit 65a87f873eba4eda93b6672d1a01397395edacb6.
    
    * TA-44: Use correct key values when requesting account info change

--- client/src/Pages/EditProfile.jsx
+++ client/src/Pages/EditProfile.jsx
@@ -62,8 +62,8 @@ const EditProfile = () => {
     const body = {
       email: getEmail(),
       password: newValidPassword,
-      username: username,
-      profilePic: profilePic,
+      name: username,
+      profilePictureURL: profilePic,
       coverImg: coverImg,
     };
 
@@ -75,7 +75,7 @@ const EditProfile = () => {
     };
 
     axios.put('/api/users', body, axiosConfig).then(response => {
-      if (response.data.success) {
+      if (response.status === 200) {
         // Succesfully changed account info on backend, update data on client
         setUserData(username, getEmail(), profilePic, coverImg);
         navigate('/dashboard');
