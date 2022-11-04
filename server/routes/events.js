a34e142fb83548b9b6634c381bb07a120fcacc79
Henry-Zhuo <henryzhuo@live.com>
2022-11-03 23:50:37 -0400

    TA-52: Revert "Replace all routes in events from those in postevent"
    This reverts commit 671723dc43bef2c86c0d95864d1f7509bde7ef45.

--- server/routes/events.js
+++ server/routes/events.js
@@ -10,32 +10,47 @@ const User = require('../models/User');
 // @route   POST api/events
 // @desc    Create an event for the user that is logged in
 // @access  Private
-router.post("/uploadEvent", auth, async (req, res) => {
-    const userId = req.user.id;
-    const user = await User.findById(userId);
-    if(!user) res.status(404).json({message: "Cannot get user"});
-    const userIdObj = new mongoose.mongo.ObjectId(userId);
-    req.body.creator = userIdObj;
-    const event = new Event(req.body);
-    event.save();
-    user.eventsPosted.push(event._id);
-    user.save();
-    res.status(200).json({ success: true, message: "Event uploaded" })
+router.post('/', [
+    check('title', 'Title is required').not().isEmpty(),
+    check('description', 'Description is required').not().isEmpty()
+], auth,  async (req, res) => {
+    //check for errors in the request
+    const errors = validationResult(req);
+    // if there are errors, return a 400 status and the errors
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+    }
+    
+    try {
+        //create a new event 
+        const newEvent = new Event({
+            title: req.body.title,
+            description: req.body.description,
+        });
+        //save the event to the database
+        const event = await newEvent.save();
+        //send the event back to the client
+        res.json(event);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 // @route   GET api/events
 // @desc    Get all events
 // @access  Private
-// Event page API
-router.post("/getEvents", auth, (req, res) => {
-
-    Event.find()
-    .exec( (err, events) => {
-        if (err) return res.status(400).json({success:false,err})
-
-        res.status(200).json({success:true , events})
-    } )
-
+router.get('/', auth, async (req, res) => {
+    try {
+        //find all events without any filters
+        const events = await Event.find();
+        //send the events back to the client
+        res.json(events);
+    } catch (err) {
+        console.error(err.message);
+        //if there is an error, send a 500 status
+        res.status(500).send('Server Error');
+    }
 });
 
 // @route   GET api/events/array
@@ -61,22 +76,24 @@ router.get('/array', auth, async (req, res) => {
 // @route   GET api/events/:id
 // @desc    get an event by its id
 // @access  Private
-router.get("/events_by_id", auth, (req, res) => {
-    let type = req.query.type
-    let eventIds = req.query.id
-
-    if (type === "array") {
-
+router.get('/:id', auth, async (req, res) => {
+    try {
+        const event = await Event.findById(req.params.id);
+        //if there is no event, send a 404 status
+        if (!event) {
+            return res.status(404).json({ msg: 'Event not found' });
+        }
+    
+        res.json(event);
+    } catch (err) {
+        console.error(err.message);
+        //if there is an error, send a 500 status
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Event not found' });
+        }
+        
+        res.status(500).send('Server Error');
     }
-
-
-    //we need to find the product information that belong to product Id 
-    Event.find({ '_id': { $in: eventIds } })
-        .populate('writer')
-        .exec((err, event) => {
-            if (err) return res.status(400).send(err)
-            return res.status(200).send(event)
-        })
 });
 
 // @route   PUT api/events/:id
@@ -118,15 +135,27 @@ router.put('/:id', auth, async (req, res) => {
 // @route   DELETE api/events/:id
 // @desc    Delete an event
 // @access  Private
-router.delete("/:id", auth, (req, res) => {
-    const userId = req.user.id;
-    const user = User.findById(userId);
-    if(!user) res.status(404).json({message: "Cannot get user"});
-    const eventId = req.params.id;
-    Event.findByIdAndDelete(eventId, (err, event) => {
-        if (err) return res.status(400).send(err)
-        return res.status(200).json({ success: true, message: "Event deleted" })
-    })
+router.delete('/:id', auth, async (req, res) => {
+    try {
+        //find the event by the id
+        const event = await Event.findById(req.params.id);
+        //if there is no event, send a 404 status
+        if (!event) {
+            return res.status(404).json({ msg: 'Event not found' });
+        }
+        //remove the event
+        await event.remove();
+        //send a message back to the client
+        res.json({ msg: 'Event removed' });
+    } catch (err) {
+        console.error(err.message);
+        //if there is an error, send a 500 status
+        if (err.kind === 'ObjectId') {
+            return res.status(404).json({ msg: 'Event not found' });
+        }
+
+        res.status(500).send('Server Error');
+    }
 });
 
 
